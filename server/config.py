8fc853b80adaeb70ce0aa042ed8d93a99c6154e0
Enoch2k2 <enoch2k2@hotmail.com>
2023-08-03 09:23:58 -0500

    added client

--- server/config.py
+++ server/config.py
@@ -10,11 +10,11 @@ app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 migrate = Migrate(app, db)
 
-db.init_app()
+db.init_app(app)
 
 bcrypt = Bcrypt(app)
 
